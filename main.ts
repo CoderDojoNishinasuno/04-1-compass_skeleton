34011aa2c3f4552b604f3ee326fee6410f9eae8c
CoderDojo西那須野 <131562680+CoderDojoNishinasuno@users.noreply.github.com>
2025-06-30 05:25:05 +0900

    Removing blocks for skeleton

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 let 方がく = 0
 basic.forever(function () {
-    方がく = input.compassHeading()
+    方がく = 0
     if (方がく < 45) {
-        basic.showArrow(ArrowNames.North)
+    	
     } else if (方がく < 135) {
-        basic.showArrow(ArrowNames.West)
+    	
     } else if (方がく < 225) {
-        basic.showArrow(ArrowNames.South)
+    	
     } else if (方がく < 315) {
-        basic.showArrow(ArrowNames.East)
+    	
     } else {
-        basic.showArrow(ArrowNames.North)
+    	
     }
 })
